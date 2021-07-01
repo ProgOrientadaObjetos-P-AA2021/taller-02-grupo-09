75491702cbf57cd32f2d83530c805d7ee2ef9217
JoseCordova11 <73542968+JoseCordova11@users.noreply.github.com>
2021-07-01 15:38:32 -0500

    cambio 1

--- ProyectoTaller/src/paquete2/PasajeInterCantonal.java
+++ ProyectoTaller/src/paquete2/PasajeInterCantonal.java
@@ -13,6 +13,79 @@ import java.io.Serializable;
  */
 public abstract class PasajeInterCantonal implements Serializable{
     
+    protected String nombre;
+    protected String ciudad;
+    protected String cedula;
+    protected String origen;
+    protected String destino;
+    protected double km;
+    protected double tarifaBase;
+    protected double valorPasaje;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public void setTarifaBase(double tarifaBase) {
+        this.tarifaBase = tarifaBase;
+    }
+
+    public double getValorPasaje() {
+        return valorPasaje;
+    }
+
+    public abstract void setValorPasaje();
+        
+    
+    
     
     
     @Override
